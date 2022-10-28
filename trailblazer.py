330f8f8e6fd834d8f2bfab5a29d1dba652e923bf
DerHeimataerde <43113297+DerHeimataerde@users.noreply.github.com>
2022-10-28 03:52:01 -0500

    Update trailblazer.py

--- trailblazer.py
+++ trailblazer.py
@@ -1,5 +1,6 @@
 import ctypes
 import mmap
+from platform import java_ver
 import time
 import xmltodict
 import win32.win32gui
@@ -7,7 +8,7 @@ import re
 import numpy as np
 import sys
 from pynput.keyboard import Key, Controller, Listener
-import os.path
+import os
 from os import path
 
 class WindowMgr:
@@ -54,10 +55,10 @@ class Vector:
     def angle_between(v1, v2):
         """ Returns the angle in degrees between vectors 'v1' and 'v2'::
         """
-        #v1_u = Vector.unit_vector(v1)
-        #v2_u = Vector.unit_vector(v2)
-        v1_angle = Vector.cart2pol(v1) #np.arctan(v1_u[1]/v1_u[0])
-        v2_angle = Vector.cart2pol(v2) #np.arctan(v2_u[1]/v2_u[0])
+        v1_2d = [v1[0],v1[1]]
+        v2_2d = [v2[0],v2[1]]
+        v1_angle = Vector.cart2pol(v1_2d) #np.arctan(v1_u[1]/v1_u[0])
+        v2_angle = Vector.cart2pol(v2_2d) #np.arctan(v2_u[1]/v2_u[0])
         theta = (v1_angle - v2_angle)
 
         if (np.degrees(theta) < 0):
@@ -137,13 +138,28 @@ class MumbleLink:
 
 class Movement:
     filepath = ""
+    interactflag = 1
     route = []
     markerPos = []
     markerVec = []
+    prevPos = []
     currentPos = []
     currentVec = []
     theta = 0
     distance = 0
+    moveTime = 0
+    speed = 0
+    speedAvg = 0
+    speedMult = 1
+    speedPrevList = []
+    hasJumped = 0
+
+    state = {
+        "gliding":0,
+        "jumping":0,
+        "correctionoverride":0
+    }
+
     ml = MumbleLink()
     k = Controller()
     w = WindowMgr()
@@ -154,16 +170,38 @@ class Movement:
             data_dict = xmltodict.parse(xml_file.read())
         self.filepath = file
         self.route = list(data_dict['OverlayData']['POIs']['POI'])
+        if (path.splitext(path.basename(self.filepath))[0] == "test"):
+            self.interactflag = 0
 
     def read(self, index):
         #populates variables with respect to next marker from route list
         self.ml.read()
-        self.markerPos = [float(self.route[index]['@xpos']), float(self.route[index]['@zpos'])]
-        self.currentPos = [self.ml.data.fAvatarPosition[0], self.ml.data.fAvatarPosition[2]]
+        self.markerPos = [float(self.route[index]['@xpos']), float(self.route[index]['@zpos']), float(self.route[index]['@ypos'])]
+        self.currentPos = [self.ml.data.fAvatarPosition[0], self.ml.data.fAvatarPosition[2], self.ml.data.fAvatarPosition[1]]
         self.currentVec = [self.ml.data.fAvatarFront[0], self.ml.data.fAvatarFront[2]]
         self.markerVec = Vector.unit_vector([(self.markerPos[0] - self.currentPos[0]), (self.markerPos[1] - self.currentPos[1])])
         self.theta = Vector.angle_between(self.currentVec, self.markerVec)
         self.distance = np.linalg.norm(np.array(self.currentPos) - np.array(self.markerPos))
+
+        if not(self.moveTime == 0) and ((np.linalg.norm(np.array(self.currentPos) - np.array(self.prevPos))) > 5): # Calculates speed and speed multiplier
+            self.speed = (np.linalg.norm(np.array(self.currentPos) - np.array(self.prevPos)))/(self.moveTime)/10
+            self.speedPrevList.insert(0,self.speed)
+            if (len(self.speedPrevList) == 5):
+                self.speedAvg = sum(self.speedPrevList)/len(self.speedPrevList)
+                self.speedPrevList.pop()
+                '''
+                if (np.abs((0.5357 - self.speedAvg)) < .02): # In Combat at Normal Speed
+                    self.speedMult = 1.0 / .5357
+                '''
+                if (np.abs((0.75 - self.speedAvg)) < .02) and (np.abs((0.75 - self.speed)) < .025): # Normal Speed
+                    self.speedMult = 1.0 / .75
+                elif (np.abs((0.9375 - self.speedAvg)) < .02) and (np.abs((0.9375 - self.speed)) < .025): # +25% Speed Buff
+                    self.speedMult = 1.0 / .9375
+                else:
+                    self.speedMult = 1.0 # +33% Speed Buff
+            else:
+                    self.speedMult = 1.0 # Default if enough speed measurements not taken
+        self.prevPos = self.currentPos
     
     def rotate(self):
         #rotates player towards next marker
@@ -185,17 +223,31 @@ class Movement:
         #moves player towards next marker
 
         key = "w"
-        moveTime = self.distance/10
+        self.moveTime = self.distance*self.speedMult/10
 
         self.k.press(key)
-        time.sleep(moveTime)
+        time.sleep(self.moveTime)
         self.k.release(key)
     
+    def glide(self, index):
+        #glides player towards next marker
+
+        key = "w"
+        self.moveTime = self.distance/10
+
+        if not(self.route[index]['@type'] == "taco.tactical.adventure.floorislava"): #prevent glider redeployment mid air 
+            self.k.press(Key.space) 
+        self.k.press(key)
+        time.sleep(self.moveTime)
+        self.k.release(key)
+        self.k.release(Key.space) 
+
     def interact(self, index):
         #checks if marker is to be interacted with or not, then does so
+
         key = "f"
-        interaction = self.route[index]['@type'] #Type of interaction
-        interactTime = 0 #Time to wait until interaction is compelete
+        interaction = self.route[index]['@type'] # Type of interaction
+        interactTime = 0 # Time to wait until interaction is compelete
 
         if (interaction == "taco"):
             interactTime = 0
@@ -211,32 +263,93 @@ class Movement:
             interactTime = 4
 
         if (interactTime > 0):
-            self.k.press(key)
-            self.k.release(key)
-            time.sleep(interactTime)
+            if self.interactflag:
+                if self.hasJumped:
+                    self.hasJumped = 0
+                    time.sleep(2)
+                self.k.press(key)
+                self.k.release(key)
+                time.sleep(interactTime)
 
+    def setstate(self, index):
+        """Sets the movement state"""
+        if (self.state["jumping"]): # If jumpflag was turned on, this turns it off
+            self.state["jumping"] = 0 
+        if (self.state["gliding"]): # If glideflag was turned on, this turns it off
+            self.state["gliding"] = 0 
+        if (self.route[index]['@type'] == "taco.jumpingpuzzle"): # Starts jumpflag to jump once from current marker to next one
+            self.state["jumping"] = 1
+        if (self.route[index]['@type'] == "taco.tactical.adventure.floorislava"): # Starts glideflag to start gliding
+            self.state["gliding"] = 1
+            
     def traverse(self):
         #Starts player movement routine
         print("Press PAUSE to start/pause. ESC to exit.")
-        for i in range(len(self.route)):
-            self.read(i)
-            count = 0
-            while (self.distance > 1):
-                while (not(WindowMgr.get_foreground() == "Guild Wars 2")): # Auto pauses inputs if GW2 is not in focus
-                    time.sleep(1)
-                while (self.pause): # pauses movement if pause key pressed
-                    time.sleep(1)
-                if count < 1:
-                    print("Moving to "+path.splitext(path.basename(self.filepath))[0]+"["+str(i)+"]["+self.route[i]['@type']+"]"+str(self.markerPos))
+
+        while(True):
+            i = 0
+            self.state["jumping"] = 0
+            self.state["gliding"] = 0
+            while (i < len(self.route))and(i>-1):
+            #for i in range(len(self.route)):
                 self.read(i)
-                self.rotate()
-                if (count > 4):
-                    self.k.press(Key.space) # If character stuck on geometry, attempts to jump "over"
-                self.move()
-                count += 1
-                self.k.release(Key.space)
-                self.read(i)
-            self.interact(i)
+                count = 0
+                jumpflag = 0
+                if (self.state["jumping"]):
+                    jumpflag = 1
+
+                while ((self.distance > 1)):
+                    while (not(WindowMgr.get_foreground() == "Guild Wars 2")): # Auto pauses inputs if GW2 is not in focus
+                        time.sleep(1)
+                    while (self.pause): # pauses movement if pause key pressed
+                        time.sleep(1)
+                    if count < 1:
+                        if i < 1:
+                            print("Route START")
+                        print("Moving to "+path.splitext(path.basename(self.filepath))[0]+"["+str(i)+"]["+self.route[i]['@type']+"]"+str(self.markerPos)+"[Speed: "+str(self.speed)+", SpeedAvg: "+str(self.speedAvg)+", SpeedMult: "+str(self.speedMult)+"]")
+
+                    self.read(i)
+                    
+                    self.rotate()
+                    if self.state["gliding"]:
+                        if not(self.route[i]['@type'] == "taco.tactical.adventure.floorislava"): # Double verify angle only glider deployment and not during gliding
+                            time.sleep(.1)
+                            self.read(i)
+                            self.rotate()
+                        self.speedMult = 1.0
+                        self.glide(i)
+                    else:
+                        time.sleep(.1)
+                        self.read(i)
+                        self.rotate() # Double verify angle
+
+                        if ((count > 4)or jumpflag): # If character stuck on geometry, attempts to jump "over"
+                            self.k.press(Key.space) 
+                            self.k.release(Key.space)
+                            self.speedMult = 1.0
+                            jumpflag = 0    
+                            self.hasJumped = 1
+                        if (count > 10): # If character can't jump over geometry, backtrack to previous node
+                            i-=1
+                            count=0
+                            print("BACKTRACKING")
+                        self.move()
+
+                    count += 1
+                    self.read(i)
+                    self.moveTime = 0
+                    if (self.state["gliding"] and (self.route[i]['@type'] == "taco.tactical.adventure.floorislava")): 
+                        # Prevents a glide loop on the same marker if marker is a "glide" marker
+                        break
+
+                self.interact(i) # Player interaction
+
+                self.setstate(i) # movement state
+
+                i+=1
+            if not(self.route[len(self.route)-1]['@type'] == "wvw.resource"):
+                break    #Loop if last marker is wvw.resource
+            print("Route LOOP")
         self.k.press(Key.space) # Jump at the end to signal route end
         self.k.release(Key.space)
         print("Route END")
@@ -259,6 +372,11 @@ def on_press(key, movement):
             os._exit(1)
 
 def main():
+    #check if run as admin
+    if not(ctypes.windll.shell32.IsUserAnAdmin()):
+        print("Application requires running as Admin to function")
+        os.system('pause')
+        os._exit(1)
     #check if xml path exists
     if len(sys.argv) < 2:
         file = input('Enter XML path: ')
@@ -266,11 +384,10 @@ def main():
         file = sys.argv[1]
     if not(path.exists(file)):
         print("Path does not exist")
+        os.system('pause')
         os._exit(1)
 
-    #ml = MumbleLink()
     mv = Movement(file)
-    w = WindowMgr()
     l = Listener(on_press=lambda event: on_press(event, movement=mv))
     l.start()
     
@@ -287,6 +404,7 @@ def main():
     #Check if map is correct
     if not(mv.map()):
         print("Wrong map")
+        os.system('pause')
         exit()
 
     #Set GW2 to active window
@@ -297,6 +415,7 @@ def main():
     mv.traverse()
     mv.ml.close()
     #l.join()
+    os.system('pause')
 
 if __name__ == "__main__":
     main()
